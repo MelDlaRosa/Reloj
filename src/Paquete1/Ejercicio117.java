2b9dbb2a8b396d48dfa8f2b85399c4bf9cabd6dd
JARLES <JARLES@Jarles-PC>
2019-08-18 11:11:27 -0500

    editado

---
+++ src/Paquete1/Ejercicio117.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JARLES
+ */
+public class Ejercicio117 {
+    public static void main (String [] args){
+        Scanner entrada = new Scanner(System.in);
+        String palabra="";
+        String vocal="";
+        int conteo=0;
+        System.out.println("Escriba la palabra: ");
+        palabra= entrada.next();
+        
+        for(int x=0; x<palabra.length();x++){
+           if(palabra.charAt(x)=='a'){
+               conteo++;
+           }
+           if(palabra.charAt(x)=='e'){
+               conteo++;
+           }
+           if(palabra.charAt(x)=='i'){
+               conteo++;
+           }
+           if(palabra.charAt(x)=='o'){
+               conteo++;
+               
+        }
+           if(palabra.charAt(x)=='u'){
+               conteo++;
+}
+           
+}
+        System.out.println("El numero de vocales es: " +conteo);
+    }
+}
+
